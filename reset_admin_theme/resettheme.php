cb580de09e6ba6ab4c2602d4293d210fa57022a6
instantflorian <info@meerwinck.com>
2019-02-08 07:25:13 +0100

    Updated template name

--- reset_admin_theme/resettheme.php
+++ reset_admin_theme/resettheme.php
@@ -4,7 +4,7 @@
 2. Run it once by calling the url http://yourserver/resettheme.php
 3. Delete the file from the server.
 
-Your admin now should be reset to the default WB_Theme.
+Your admin now should be reset to the default backend theme.
 
 */
 
@@ -13,14 +13,13 @@ Your admin now should be reset to the default WB_Theme.
 
 include ('config.php');
 
-//$theme="wb_theme";  //WBclassic
-$theme="advancedThemeWbFlat"; //WBCE
+$theme="wbce_flat_theme"; 
 
 $database->query( "UPDATE ".TABLE_PREFIX."settings SET `value` = '".$theme."' where `name` = 'default_theme'");
 if($database->is_error()) {
 	echo "Error: ".$database->get_error();
 } else {
-	echo "Succesfully reset default Admin-Theme to 'wb_theme'";
+	echo "Succesfully reset default Admin-Theme to ".$theme.", please delete reset_admin_theme.php from the server";
 }
 
 
