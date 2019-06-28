2c89b8cbc3389dfde5b2da8f882eaaf0d5bf1604
Martin Hecht <mrbaseman@gmx.de>
2019-06-28 10:45:04 +0200

    Revert "merge latest version of get_client_ip from WBCE_cms into unblock_source_ip"
    This reverts commit d82a9a058dc384071b3b9db6e8941204c20d7d92.

--- unblock_source_ip/unblock_source_ip.php
+++ unblock_source_ip/unblock_source_ip.php
@@ -16,25 +16,24 @@ be able to see the login mask again.
 
 include ('config.php');
 
-    private function get_client_ip()
-    {
-        $ipaddress = '';
-	if (isset($_SERVER['HTTP_CLIENT_IP']))           $ipaddress = $this->get_server('HTTP_CLIENT_IP');
-	else if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $ipaddress = $this->get_server('HTTP_X_FORWARDED_FOR');
-	else if(isset($_SERVER['HTTP_X_FORWARDED']))     $ipaddress = $this->get_server('HTTP_X_FORWARDED');
-	else if(isset($_SERVER['HTTP_FORWARDED_FOR']))   $ipaddress = $this->get_server('HTTP_FORWARDED_FOR');
-	else if(isset($_SERVER['HTTP_FORWARDED']))       $ipaddress = $this->get_server('HTTP_FORWARDED');
-	else if(isset($_SERVER['REMOTE_ADDR']))          $ipaddress = $this->get_server('REMOTE_ADDR');
-	else if(getenv('HTTP_CLIENT_IP'))                $ipaddress = getenv('HTTP_CLIENT_IP');
-	else if(getenv('HTTP_X_FORWARDED_FOR'))          $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-	else if(getenv('HTTP_X_FORWARDED'))              $ipaddress = getenv('HTTP_X_FORWARDED');
-	else if(getenv('HTTP_FORWARDED_FOR'))            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-	else if(getenv('HTTP_FORWARDED'))                $ipaddress = getenv('HTTP_FORWARDED');
-	else if(getenv('REMOTE_ADDR'))                   $ipaddress = getenv('REMOTE_ADDR');
-	else                                             $ipaddress = 'UNKNOWN';
-
-	return $ipaddress;
-    }
+function get_client_ip()
+{
+    $ipaddress = ''; 
+    if ($_SERVER['HTTP_CLIENT_IP']) $ipaddress = $_SERVER['HTTP_CLIENT_IP']; 
+    else if($_SERVER['HTTP_X_FORWARDED_FOR']) $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    else if($_SERVER['HTTP_X_FORWARDED']) $ipaddress = $_SERVER['HTTP_X_FORWARDED']; 
+    else if($_SERVER['HTTP_FORWARDED_FOR']) $ipaddress = $_SERVER['HTTP_FORWARDED_FOR']; 
+    else if($_SERVER['HTTP_FORWARDED']) $ipaddress = $_SERVER['HTTP_FORWARDED']; 
+    else if($_SERVER['REMOTE_ADDR']) $ipaddress = $_SERVER['REMOTE_ADDR']; 
+    else if(getenv('HTTP_CLIENT_IP')) $ipaddress = getenv('HTTP_CLIENT_IP'); 
+    else if(getenv('HTTP_X_FORWARDED_FOR')) $ipaddress = getenv('HTTP_X_FORWARDED_FOR'); 
+    else if(getenv('HTTP_X_FORWARDED')) $ipaddress = getenv('HTTP_X_FORWARDED'); 
+    else if(getenv('HTTP_FORWARDED_FOR')) $ipaddress = getenv('HTTP_FORWARDED_FOR'); 
+    else if(getenv('HTTP_FORWARDED')) $ipaddress = getenv('HTTP_FORWARDED'); 
+    else if(getenv('REMOTE_ADDR')) $ipaddress = getenv('REMOTE_ADDR'); 
+    else $ipaddress = 'UNKNOWN';  
+    return $ipaddress; 
+}
  
      
 $client_ip = get_client_ip();
